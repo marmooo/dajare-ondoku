53b7328f561b0836be1860965b618480af65c7aa
marmooo <marmooo@users.noreply.github.com>
2025-07-03 00:03:15 +0900

    improve unlock

--- src/index.js
+++ src/index.js
@@ -58,10 +58,6 @@ function createAudioContext() {
 }
 
 function unlockAudio() {
-  const uttr = new SpeechSynthesisUtterance("");
-  uttr.lang = "ja-JP";
-  speechSynthesis.speak(uttr);
-
   if (audioContext) {
     audioContext.resume();
   } else {
@@ -514,6 +510,7 @@ function initTime() {
 }
 
 function countdown() {
+  speak(""); // unlock
   gameStart.classList.remove("d-none");
   playPanel.classList.add("d-none");
   scorePanel.classList.add("d-none");
